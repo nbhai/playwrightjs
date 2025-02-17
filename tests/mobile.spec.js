77d14eb18de540d61d3ebfbbe33e3037938c3353
hai nguyen <hai.nguyenbac@emeal.nttdata.com>
2025-02-17 14:54:42 +0700

    update files

--- tests/mobile.spec.js
+++ tests/mobile.spec.js
@@ -1,15 +1,15 @@
-const { test, devices } = require('@playwright/test');
+// const { test, devices } = require('@playwright/test');
 
-test.use({
-  ...devices['iPhone 13 Pro'],
-  locale: 'en-US',
-  geolocation: { longitude: 12.492507, latitude: 41.889938 },
-  permissions: ['geolocation'],
-});
+// test.use({
+//   ...devices['iPhone 13 Pro'],
+//   locale: 'en-US',
+//   geolocation: { longitude: 12.492507, latitude: 41.889938 },
+//   permissions: ['geolocation'],
+// });
 
-test('Mobile and geolocation', async ({ page }) => {
-  await page.goto('https://maps.google.com');
-  await page.locator('text=Your location').click();
-  await page.waitForRequest(/.*preview\/pwa/);
-  await page.screenshot({ path: 'colosseum-iphone.png' });
-});
+// test('Mobile and geolocation', async ({ page }) => {
+//   await page.goto('https://maps.google.com');
+//   await page.locator('text=Your location').click();
+//   await page.waitForRequest(/.*preview\/pwa/);
+//   await page.screenshot({ path: 'colosseum-iphone.png' });
+// });
--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,14 +1,14 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.amazon.com/');
-  await expect(page.getByRole('link', { name: 'Amazon', exact: true })).toBeVisible();
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).fill('iphone 15');
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).press('Enter');
-  await expect(page.getByRole('link', { name: 'Apple iPhone 15, 128GB, Blue' })).toBeVisible();
-  await page.getByRole('link', { name: 'Apple iPhone 15, 128GB, Blue' }).click();
-  await expect(page.locator('#ppd')).toBeVisible();
-  await page.getByRole('button', { name: '256GB' }).click();
-  await expect(page.locator('#availability').getByText('This item cannot be shipped')).toBeVisible();
-});
+// test('test', async ({ page }) => {
+//   await page.goto('https://www.amazon.com/');
+//   await expect(page.getByRole('link', { name: 'Amazon', exact: true })).toBeVisible();
+//   await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
+//   await page.getByRole('searchbox', { name: 'Search Amazon' }).fill('iphone 15');
+//   await page.getByRole('searchbox', { name: 'Search Amazon' }).press('Enter');
+//   await expect(page.getByRole('link', { name: 'Apple iPhone 15, 128GB, Blue' })).toBeVisible();
+//   await page.getByRole('link', { name: 'Apple iPhone 15, 128GB, Blue' }).click();
+//   await expect(page.locator('#ppd')).toBeVisible();
+//   await page.getByRole('button', { name: '256GB' }).click();
+//   await expect(page.locator('#availability').getByText('This item cannot be shipped')).toBeVisible();
+// });
